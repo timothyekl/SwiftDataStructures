534981666da6e8999352d7ad66c9b84a0c6fe3d1
Tim Ekl <lithium3141@gmail.com>
2014-06-02 20:14:14 -0700

    Initial Commit

---
+++ SwiftDataStructures/SwiftDataStructures.h
@@ -0,0 +1,19 @@
+//
+//  SwiftDataStructures.h
+//  SwiftDataStructures
+//
+//  Created by Tim Ekl on 6/2/14.
+//  Copyright (c) 2014 Tim Ekl. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SwiftDataStructures.
+FOUNDATION_EXPORT double SwiftDataStructuresVersionNumber;
+
+//! Project version string for SwiftDataStructures.
+FOUNDATION_EXPORT const unsigned char SwiftDataStructuresVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftDataStructures/PublicHeader.h>
+
+
